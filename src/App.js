79b85b353d09320a3c58ad0c361d9166db0516d4
tsipherov <tsipherov@gmail.com>
2023-03-03 20:31:36 +0200

    implemented base routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactsPage from "./pages/ContactsPage";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Header />
+      <main className="container content">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
